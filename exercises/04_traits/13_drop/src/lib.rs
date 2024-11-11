5c38dab2c76b8f5fc288c87a62150e19240f406e
sdhornet <76028034+sdhornet@users.noreply.github.com>
2024-11-10 19:56:59 -0600

    Drop

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,17 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+// This is a unit struct, i.e. a struct with no fields.
+#[derive(Clone, Copy)]
+struct DropBomb;
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        // We don't need to do anything here,
+        // it's enough to have an "empty" Drop implementation
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
