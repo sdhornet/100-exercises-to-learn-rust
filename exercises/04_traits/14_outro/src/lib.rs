e33273e6e9535de958684c35c79ad226544a3063
sdhornet <76028034+sdhornet@users.noreply.github.com>
2024-11-11 18:21:02 -0600

    new solves

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -9,6 +9,73 @@
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
 
-pub struct SaturatingU16 {
-    value: u16,
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct SaturatingU16(u16);
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16(value)
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16(value as u16)
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16(*value)
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16(*value as u16)
+    }
+}
+
+impl std::ops::Add for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        SaturatingU16(self.0.saturating_add(other.0))
+    }
+}
+
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: u16) -> Self {
+        SaturatingU16(self.0.saturating_add(other))
+    }
+}
+
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: &u16) -> Self {
+        SaturatingU16(self.0.saturating_add(*other))
+    }
+}
+
+impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: &SaturatingU16) -> Self {
+        SaturatingU16(self.0.saturating_add(other.0))
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.0 == *other
+    }
+}
+
+impl From<SaturatingU16> for u16 {
+    fn from(value: SaturatingU16) -> Self {
+        value.0
+    }
 }
